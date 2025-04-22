c3de2910a1f371d34bebfc9afb13f0d567b89720
Hannah Osei <hannah.osei@softwire.com>
2025-04-22 15:36:39 +0100

    Multiples of 3 and 5 now print 'FizzBuzz'

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,14 @@
 function fizzBuzz() {
     let number = 1;
     while (number <= 100) {
-        if (number % 3 === 0) {
+        if (number % 3 === 0 && number % 5 === 0) {
+            console.log("FizzBuzz");
+        }  
+        else if (number % 3 === 0) {
             console.log("Fizz");
         }
         else if (number % 5 === 0) {
             console.log("Buzz");
-        }
-        else if (number % 3 === 0 && 5 === 0) {
-            console.log("FizzBuzz");
         }
         else {
             console.log(number);
