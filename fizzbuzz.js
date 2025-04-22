57e2bf597ea8a33d9788e62d406f8e67bac43fcb
Hannah Osei <hannah.osei@softwire.com>
2025-04-22 15:22:44 +0100

    Prints numbers 1 - 100

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,9 @@
 function fizzBuzz() {
     let number = 1;
-    
     while (number <= 100) {
-        console.log(number); number ++;
-        if (number % 3 === 0) {
-            console.log("Fizz");
-        }
-        if (number % 5 === 0) {
-            console.log("Buzz");
-        }
-        if (number % 3 === 0 && 5 === 0) {
-            console.log("FizzBuzz");
-        }
+        console.log(number);
+        number ++;
     }
 }
 
-fizzBuzz()
+fizzBuzz();
