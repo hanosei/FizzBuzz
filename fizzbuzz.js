04c5bfb28d422beb16754cf4dc9e7fa1c1d8507d
Hannah Osei <hannah.osei@softwire.com>
2025-04-22 15:27:49 +0100

    Prints 'Fizz' for multiples of 3 and 'Buzz' for multiples of 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,18 @@
 function fizzBuzz() {
     let number = 1;
     while (number <= 100) {
-        console.log(number);
+        if (number % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (number % 5 === 0) {
+            console.log("Buzz");
+        }
+        else if (number % 3 === 0 && 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else {
+            console.log(number);
+        }
         number ++;
     }
 }
