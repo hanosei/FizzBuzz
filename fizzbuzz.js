3dd41257b89eb761d0df5daf1f20b2a132ed4bc7
Hannah Osei <hannah.osei@softwire.com>
2025-04-22 15:03:09 +0100

    FizzBuzz to appear next to multiples of 3 and 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,6 +3,15 @@ function fizzBuzz() {
     
     while (number <= 100) {
         console.log(number); number ++;
+        if (number % 3 === 0) {
+            console.log("Fizz");
+        }
+        if (number % 5 === 0) {
+            console.log("Buzz");
+        }
+        if (number % 3 === 0 && 5 === 0) {
+            console.log("FizzBuzz");
+        }
     }
 }
 
