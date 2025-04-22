503f0f9ea8ddb83ca6a55f4cce93a7106f6b82bd
Hannah Osei <hannah.osei@softwire.com>
2025-04-22 15:50:27 +0100

    multiples of 11 and 13 print Bong and Fezz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,13 +3,19 @@ function fizzBuzz() {
     while (number <= 100) {
         if (number % 3 === 0 && number % 5 === 0) {
             console.log("FizzBuzz");
-        }
-        else if (number % 7 === 0 && (number % 3 === 0 || number % 5 === 0)) {
+        }      
+        else if (number % 7 ===0 && (number % 3 === 0 || number % 5 === 0)) {
             console.log('FizzBang');
+        }
+        else if (number % 11 === 0) {
+            console.log("Bong")
+        }
+        else if (number % 13 === 0) {
+            console.log("Fezz")
         }
         else if (number % 3 === 0) {
             console.log("Fizz");
-        }
+        } 
         else if (number % 5 === 0) {
             console.log("Buzz");
         }
